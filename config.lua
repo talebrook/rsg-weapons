ba53de0d2ff239bfab82db39a4523af9ed03f20f
Talebrook Roleplay <talebrookroleplay@gmail.com>
2025-11-17 23:09:26 +0100

    rsg-weapons frissítve

--- config.lua
+++ config.lua
@@ -19,70 +19,70 @@ Config.DisableSprintWhileAiming = true
 Config.DisableCriticalHits = false 
 Config.WeaponDamage = {
     -- Melee
-    {Name = 'WEAPON_UNARMED', Damage = 1.0},
-    {Name = 'WEAPON_MELEE_CLEAVER', Damage = 1.0},
-    {Name = 'WEAPON_MELEE_HAMMER', Damage = 1.0},
-    {Name = 'WEAPON_MELEE_HATCHET', Damage = 1.0},
-    {Name = 'WEAPON_MELEE_HATCHET_HUNTER', Damage = 1.0},
-    {Name = 'WEAPON_MELEE_KNIFE', Damage = 1.0},
-    {Name = 'WEAPON_MELEE_KNIFE_HORROR', Damage = 1.0},
-    {Name = 'WEAPON_MELEE_KNIFE_JAWBONE', Damage = 1.0},
-    {Name = 'WEAPON_MELEE_KNIFE_RUSTIC', Damage = 1.0},
-    {Name = 'WEAPON_MELEE_KNIFE_TRADER', Damage = 1.0},
-    {Name = 'WEAPON_MELEE_MACHETE', Damage = 1.0},
-    {Name = 'WEAPON_MELEE_MACHETE_COLLECTOR', Damage = 1.0},
-    {Name = 'WEAPON_MELEE_MACHETE_HORROR', Damage = 1.0},
+    {Name = 'WEAPON_UNARMED', Damage = 1.5},
+    {Name = 'WEAPON_MELEE_CLEAVER', Damage = 1.5},
+    {Name = 'WEAPON_MELEE_HAMMER', Damage = 1.5},
+    {Name = 'WEAPON_MELEE_HATCHET', Damage = 1.5},
+    {Name = 'WEAPON_MELEE_HATCHET_HUNTER', Damage = 1.5},
+    {Name = 'WEAPON_MELEE_KNIFE', Damage = 1.5},
+    {Name = 'WEAPON_MELEE_KNIFE_HORROR', Damage = 1.5},
+    {Name = 'WEAPON_MELEE_KNIFE_JAWBONE', Damage = 1.5},
+    {Name = 'WEAPON_MELEE_KNIFE_RUSTIC', Damage = 1.5},
+    {Name = 'WEAPON_MELEE_KNIFE_TRADER', Damage = 1.5},
+    {Name = 'WEAPON_MELEE_MACHETE', Damage = 1.5},
+    {Name = 'WEAPON_MELEE_MACHETE_COLLECTOR', Damage = 1.5},
+    {Name = 'WEAPON_MELEE_MACHETE_HORROR', Damage = 1.5},
 
     -- Revolver
-    {Name = 'WEAPON_REVOLVER_CATTLEMAN', Damage = 1.0},
-    {Name = 'WEAPON_REVOLVER_CATTLEMAN_MEXICAN', Damage = 1.0},
-    {Name = 'WEAPON_REVOLVER_DOUBLEACTION', Damage = 1.0},
-    {Name = 'WEAPON_REVOLVER_DOUBLEACTION_GAMBLER', Damage = 1.0},
-    {Name = 'WEAPON_REVOLVER_LEMAT', Damage = 1.0},
-    {Name = 'WEAPON_REVOLVER_NAVY', Damage = 1.0},
-    {Name = 'WEAPON_REVOLVER_NAVY_CROSSOVER', Damage = 1.0},
-    {Name = 'WEAPON_REVOLVER_SCHOFIELD', Damage = 1.0},
+    {Name = 'WEAPON_REVOLVER_CATTLEMAN', Damage = 1.5},
+    {Name = 'WEAPON_REVOLVER_CATTLEMAN_MEXICAN', Damage = 1.5},
+    {Name = 'WEAPON_REVOLVER_DOUBLEACTION', Damage = 1.5},
+    {Name = 'WEAPON_REVOLVER_DOUBLEACTION_GAMBLER', Damage = 1.5},
+    {Name = 'WEAPON_REVOLVER_LEMAT', Damage = 1.5},
+    {Name = 'WEAPON_REVOLVER_NAVY', Damage = 1.5},
+    {Name = 'WEAPON_REVOLVER_NAVY_CROSSOVER', Damage = 1.5},
+    {Name = 'WEAPON_REVOLVER_SCHOFIELD', Damage = 1.5},
 
     -- Pistols
-    {Name = 'WEAPON_PISTOL_M1899', Damage = 1.0},
-    {Name = 'WEAPON_PISTOL_MAUSER', Damage = 1.0},
-    {Name = 'WEAPON_PISTOL_SEMIAUTO', Damage = 1.0},
-    {Name = 'WEAPON_PISTOL_VOLCANIC', Damage = 1.0},
+    {Name = 'WEAPON_PISTOL_M1899', Damage = 1.5},
+    {Name = 'WEAPON_PISTOL_MAUSER', Damage = 1.5},
+    {Name = 'WEAPON_PISTOL_SEMIAUTO', Damage = 1.5},
+    {Name = 'WEAPON_PISTOL_VOLCANIC', Damage = 1.5},
 
     -- Snipers
-    {Name = 'WEAPON_SNIPERRIFLE_CARCANO', Damage = 1.0},
-    {Name = 'WEAPON_SNIPERRIFLE_ROLLINGBLOCK', Damage = 1.0},
+    {Name = 'WEAPON_SNIPERRIFLE_CARCANO', Damage = 1.5},
+    {Name = 'WEAPON_SNIPERRIFLE_ROLLINGBLOCK', Damage = 1.5},
 
     -- Rifle
-    {Name = 'WEAPON_RIFLE_BOLTACTION', Damage = 1.0},
-    {Name = 'WEAPON_RIFLE_ELEPHANT', Damage = 1.0},
-    {Name = 'WEAPON_RIFLE_SPRINGFIELD', Damage = 1.0},
-    {Name = 'WEAPON_RIFLE_VARMINT', Damage = 1.0},
+    {Name = 'WEAPON_RIFLE_BOLTACTION', Damage = 1.5},
+    {Name = 'WEAPON_RIFLE_ELEPHANT', Damage = 1.5},
+    {Name = 'WEAPON_RIFLE_SPRINGFIELD', Damage = 1.5},
+    {Name = 'WEAPON_RIFLE_VARMINT', Damage = 1.5},
 
     -- Repeater
-    {Name = 'WEAPON_REPEATER_CARBINE', Damage = 1.0},
-    {Name = 'WEAPON_REPEATER_EVANS', Damage = 1.0},
-    {Name = 'WEAPON_REPEATER_HENRY', Damage = 1.0},
-    {Name = 'WEAPON_REPEATER_WINCHESTER', Damage = 1.0},
+    {Name = 'WEAPON_REPEATER_CARBINE', Damage = 1.5},
+    {Name = 'WEAPON_REPEATER_EVANS', Damage = 1.5},
+    {Name = 'WEAPON_REPEATER_HENRY', Damage = 1.5},
+    {Name = 'WEAPON_REPEATER_WINCHESTER', Damage = 1.5},
 
     -- Thrown
-    {Name = 'WEAPON_THROWN_DYNAMITE', Damage = 1.0},
-    {Name = 'WEAPON_THROWN_POISONBOTTLE', Damage = 1.0},
-    {Name = 'WEAPON_THROWN_THROWING_KNIVES', Damage = 1.0},
-    {Name = 'WEAPON_THROWN_TOMAHAWK', Damage = 1.0},
-    {Name = 'WEAPON_THROWN_TOMAHAWK_ANCIENT', Damage = 1.0},
-    --[[ {Name = 'WEAPON_THROWN_MOLOTOV', Damage = 1.0}, ]] -- Has no effect
+    {Name = 'WEAPON_THROWN_DYNAMITE', Damage = 1.5},
+    {Name = 'WEAPON_THROWN_POISONBOTTLE', Damage = 1.5},
+    {Name = 'WEAPON_THROWN_THROWING_KNIVES', Damage = 1.5},
+    {Name = 'WEAPON_THROWN_TOMAHAWK', Damage = 1.5},
+    {Name = 'WEAPON_THROWN_TOMAHAWK_ANCIENT', Damage = 1.5},
+    --[[ {Name = 'WEAPON_THROWN_MOLOTOV', Damage = 1.5}, ]] -- Has no effect
 
     -- Shotgun
-    {Name = 'WEAPON_SHOTGUN_DOUBLEBARREL', Damage = 1.0},
-    {Name = 'WEAPON_SHOTGUN_PUMP', Damage = 1.0},
-    {Name = 'WEAPON_SHOTGUN_REPEATING', Damage = 1.0},
-    {Name = 'WEAPON_SHOTGUN_SAWEDOFF', Damage = 1.0},
-    {Name = 'WEAPON_SHOTGUN_SEMIAUTO', Damage = 1.0},
+    {Name = 'WEAPON_SHOTGUN_DOUBLEBARREL', Damage = 1.5},
+    {Name = 'WEAPON_SHOTGUN_PUMP', Damage = 1.5},
+    {Name = 'WEAPON_SHOTGUN_REPEATING', Damage = 1.5},
+    {Name = 'WEAPON_SHOTGUN_SAWEDOFF', Damage = 1.5},
+    {Name = 'WEAPON_SHOTGUN_SEMIAUTO', Damage = 1.5},
 
     -- Bows
-    {Name = 'WEAPON_BOW', Damage = 1.0},
-    {Name = 'WEAPON_BOW_IMPROVED', Damage = 1.0},
+    {Name = 'WEAPON_BOW', Damage = 1.5},
+    {Name = 'WEAPON_BOW_IMPROVED', Damage = 1.5},
 }
 
 Config.ThrowableWeaponAmmoTypes = {
